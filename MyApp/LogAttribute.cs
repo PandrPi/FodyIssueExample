c3ffa58145f9963f4fef2ba165a6a6f0baf81f9c
pavlo.andrushko <pavlo.andrushko.me@gmail.com>
2023-03-06 22:32:02 +0200

    Initial commit

---
+++ MyApp/LogAttribute.cs
@@ -0,0 +1,21 @@
+﻿using MethodBoundaryAspect.Fody.Attributes;
+
+namespace MyApp;
+
+public sealed class LogAttribute : OnMethodBoundaryAspect
+{
+    public override void OnEntry(MethodExecutionArgs args)
+    {
+        Console.WriteLine("On entry");
+    }
+
+    public override void OnExit(MethodExecutionArgs args)
+    {
+        Console.WriteLine("On exit");
+    }
+
+    public override void OnException(MethodExecutionArgs args)
+    {
+        Console.WriteLine("On exception");
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MyApp;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var temp = Program.GetRandomInteger();
+
+        Console.WriteLine("Hello, World! {0}", temp);
+    }
+
+    [Log]
+    private static int GetRandomInteger()
+    {
+        return Random.Shared.Next();
+    }
+}
